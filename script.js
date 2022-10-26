ae17389296a775dc81b5ee2941b792ed9d6e0949
sonukuldeep <sonukuldeep@outlook.com>
2022-10-26 21:09:24 +0530

    group chat layout done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function activateList() {
     // console.log(signin_out.children[0])
 }
 
-
+//log in log out with google
 const signin_out = document.querySelector("#signIn-signOut")
 signin_out.addEventListener("click", ()=>{callGoogleSignIn()})
 function callGoogleSignIn(){
@@ -15,8 +15,18 @@ function callGoogleSignIn(){
         logout()
     } else {
         googleSignIn()
-        console.log("fire")
 
     }
-    // e.preventDefault()
+}
+
+const group_chat = document.querySelector("#signIn-signOut")
+signin_out.addEventListener("click", ()=>{callGoogleSignIn()})
+function callGoogleSignIn(){
+    const signoutButton = document.querySelectorAll('.signIn')[0]
+    if(signoutButton.classList.contains("logged-in")) {
+        logout()
+    } else {
+        googleSignIn()
+
+    }
 }
