6a95be2f269ba71dd3bb993d7d017c0d3082d0cc
sonukuldeep <sonukuldeep@outlook.com>
2022-11-04 12:57:27 +0530

    handshake and functionaing achieved

--- privateChat/script.js
+++ privateChat/script.js
@@ -1,14 +1,12 @@
-import { addToPrivateDB, privateChat } from '../Config/firebaseConfig.js'
-//addToPrivateDB()
+import { addToPrivateDB, privateChat, addToRealTimeDB } from '../Config/firebaseConfig.js'
 
 const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString);
-const chatroomID = urlParams.get('chatroomID')
-const chatroomData = urlParams.get('chatroomData')
+const urlParams = new URLSearchParams(queryString)
+const urlParamsArray = Array.from(urlParams.values())
 
-if(chatroomID !== null && chatroomData !== null) {
-    sessionStorage.setItem("userData", JSON.stringify({ "chatroomID": chatroomID, "chatroomData": chatroomData }))
-    console.log(chatroomData,chatroomID)
+if (queryString.length !== 0) {
+    sessionStorage.setItem("userData", JSON.stringify({ "chatroomID": urlParamsArray[0], "chatroomData": urlParamsArray[1] }))
+    console.log(`sessionStorage set to ${urlParamsArray[0]}`)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,12 +18,31 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const copyToClip = () => {
-    const rootID = document.querySelector('[data-url]')
-    rootID.style.cursor = 'pointer'
-    rootID.addEventListener('click', () => {
 
-        // Copy the text inside the text field
-        navigator.clipboard.writeText(rootID.getAttribute('data-url'));
-        console.log("copied")
-    })
+    if (queryString.length === 0) {
+
+        const rootID = document.querySelector('[data-url]')
+        rootID.style.cursor = 'pointer'
+        rootID.addEventListener('click', () => {
+
+            // Copy the text inside the text field
+            navigator.clipboard.writeText(rootID.getAttribute('data-url'));
+            console.log("copied", rootID.getAttribute('data-url'))
+        })
+    }
 }
+
+const msg = document.querySelector("#userMsg")
+const btnTrigger = document.querySelector(".sendBtn")
+msg?.addEventListener("keypress", (event) => { if (event.key === "Enter") { event.preventDefault(); sendMsg() } })
+btnTrigger?.addEventListener("click", () => { sendMsg() })
+function sendMsg() {
+    addToRealTimeDB(msg.value, 'private')
+    console.log(msg.value)
+    msg.value = ""
+}
+
+window.addEventListener('beforeunload', (event) => {
+    event.returnValue = `Are you sure you want to leave?`;
+  });
+  
