ac8c671a33a67fdebc2b998233697846d23dfaf1
sonukuldeep <sonukuldeep@outlook.com>
2022-11-10 23:19:06 +0530

    fixed few bugs with responsiveness

--- gChat.js
+++ gChat.js
@@ -8,31 +8,4 @@ export const displaySubscript = () => {
     })
 }
 
-// export const contextMenu = () => {
-
-//     const contextTrigger = document.querySelectorAll(".contextTriggerBtn")
-
-//     contextTrigger.forEach(btn => {
-//         const contextMenu = document.createElement('div')
-//         contextMenu.classList.add("contextMenu")
-//         contextMenu.innerHTML = `<div>
-//             <ul>
-//                 <li><button>Send private message invite</button></li>
-//                 <li><button>Send friend request</button></li>
-//                 <li><button>Follow user</button></li>
-//                 <li><button>Report</button></li>
-//             </ul>
-//         </div>`
-        
-//         btn.append(contextMenu)
-//         btn.addEventListener("click", (e) => {
-//             e.preventDefault()
-//             contextMenu.classList.toggle("visible")
-//         })
-
-//     })
-
-
-// }
-
 
