70852d3729f410ad717e326e797fcb6d1b0aca21
sonukuldeep <sonukuldeep@outlook.com>
2022-11-14 10:00:32 +0530

    pointing to database

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -18,8 +18,8 @@ const firebaseConfig = {
 
 
 //my server url. Helps hide apis and assign them when needed
-const url = "https://gleaming-pink-pea-coat.cyclic.app/api"
-// const url = "https://gleaming-pink-pea-coat.cyclic.app"
+// const url = "http://localhost:5000/api"
+const url = "https://gleaming-pink-pea-coat.cyclic.app"
 
 const apiCall = async () => {
     const rawApi = await fetch(url)
